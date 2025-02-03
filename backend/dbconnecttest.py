9c9b0fcfd3a8f9eb1fb3b6da93c0ad63c373083c
benjaminstrandberg <73587010+benjaminstrandberg@users.noreply.github.com>
2025-02-03 14:44:26 +0100

    testing

---
+++ backend/dbconnecttest.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+import pymysql
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def connect_db():
+    try:
+        connect = pymysql.connect(
+            host="mysql",
+            user = "root",
+            password = os.getenv("MYSQL_ROOT_PASSWORD"),
+            database="testdb",
+            port=3306
+        )
+        with connect.cursor() as cursor:
+            cursor.execute("SELECT * FROM users;")
+            result = cursor.fetchall()
+            
+        return {"message": "Connected to MySQL", "data": result}
+
+    except Exception as e:
+        return {"error": str(e)}
+        
+
+    
