7b36ca8019c729f45f9d48a02e683b4853fdc5a0
benjaminstrandberg <73587010+benjaminstrandberg@users.noreply.github.com>
2025-02-04 00:39:45 +0100

    initial backend setup

--- backend/dbconnecttest.py
+++ backend/dbconnecttest.py
@@ -23,6 +23,22 @@ def connect_db():
     except Exception as e:
         return {"error": str(e)}
         
+@app.get("/test2")
+def connect_db():
+    try:
+        connect = pymysql.connect(
+            host="mysql",
+            user = "root",
+            password = os.getenv("MYSQL_ROOT_PASSWORD"),
+            database="testdb",
+            port=3306
+        )
+        with connect.cursor() as cursor:
+            cursor.execute("SELECT * FROM users;")
+            result = cursor.fetchall()
+            
+        return {"message": "Connected to MySQL number 2", "data": result}
 
+    except Exception as e:
+        return {"error": str(e)}
     
-    #ha
