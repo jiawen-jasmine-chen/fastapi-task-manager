a27d43b4abaf3818f1bd0ae88014208044672e26
Stella0233 <yrotk195@gmail.com>
2025-03-06 17:08:33 +0100

    route fixing pt.2

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -23,6 +23,8 @@ function AppTabs() {
             iconName = "home-outline";
           } else if (route.name === "CalendarScreen") {
             iconName = "calendar-outline";
+          } else if(route.name === "ListScreen"){
+            iconName = "list-outline";
           }
           return <Ionicons name={iconName as keyof typeof Ionicons.glyphMap} size={size} color={color} />;
         },
@@ -32,6 +34,7 @@ function AppTabs() {
     >
       {/* ✅ 只在底部导航栏显示 Home 和 Calendar */}
       <Tabs.Screen name="HomeScreen" options={{ title: "Home" }} />
+      <Tabs.Screen name="ListScreen" options={{ title: "List" }} />
       <Tabs.Screen name="CalendarScreen" options={{ title: "Calendar" }} />
 
       {/* ❌ 隐藏非底部导航的页面 */}
@@ -39,7 +42,7 @@ function AppTabs() {
       <Tabs.Screen name="TaskDetailScreen" options={{ href: null }} />
       <Tabs.Screen name="LoginScreen" options={{ href: null }} />
       <Tabs.Screen name="RegisterScreen" options={{ href: null }} />
-      <Tabs.Screen name="ListScreen" options={{ href: null }} />
+      {/* <Tabs.Screen name="ListScreen" options={{ href: null }} /> */}
       <Tabs.Screen name="(tabs)" options={{ href: null }} />
       <Tabs.Screen name="+not-found" options={{ href: null }} />
       <Tabs.Screen name="index" options={{ href: null }} />
