e32dec269d8d9b9204d7fe75e63f6f9f8dac690d
Stella0233 <yrotk195@gmail.com>
2025-03-07 10:29:33 +0100

    opimized the layout of todolists

--- frontend/styles/listStyles.tsx
+++ frontend/styles/listStyles.tsx
@@ -1,19 +1,19 @@
 import { StyleSheet } from 'react-native';
 
 const listStyles = StyleSheet.create({
-    listContainer: {
-      padding: 15,
-      margin: 10,
-      borderRadius: 8,
-      backgroundColor: '#f8f9fa', // 默认背景色
-      flexDirection: 'row',
-      alignItems: 'center',
-    },
+    // listContainer: {
+    //   padding: 15,
+    //   margin: 10,
+    //   borderRadius: 8,
+    //   backgroundColor: '#f8f9fa', // 默认背景色
+    //   flexDirection: 'row',
+    //   alignItems: 'center',
+    // },
     sharedList: {
       backgroundColor: '#E6E6FA', // 共享列表的特殊背景色
     },
     shareIcon: {
-      marginLeft: 10,
+      marginLeft: 5,
     },
     mainContainer: {
       paddingHorizontal: 20,
@@ -30,6 +30,29 @@ const listStyles = StyleSheet.create({
       textAlign: 'center',
       paddingVertical: 20,
     },
+    // listHeader: {
+    //   flexDirection: "row",
+    //   justifyContent: "space-between",
+    //   alignItems: "center",
+    // },
+    listContainer: {
+      marginVertical: 10,
+      padding: 10,
+      backgroundColor: "#f8f9fa",
+      borderRadius: 10,
+    },
+    listHeader: {
+      flexDirection: "row",
+      justifyContent: "space-between", // 标题和按钮分开
+      alignItems: "center",
+      paddingBottom: 5, // 确保列表与标题有间距
+    },
+    // listTitle: {
+    //   fontSize: 18,
+    //   fontWeight: "bold",
+    //   color: "black",
+    //   flexShrink: 1, // 避免过长标题挤压按钮
+    // },
   });
 
   export default listStyles;
